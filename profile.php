a3a792aa5ce1d1647be4e8323a7bb8cba89d4a29
rattanachai007 <87803233+rattanachai007@users.noreply.github.com>
2021-07-25 18:02:23 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'oAhhYNK3M9EIIMOOMf2UvGtH6P+WATAmgxO+S8db8SAKMP4BX/2zFpaAALu6fSNfyDMdsMn0VYYjc8kY+2X7zbwOouCcJsEO/Qc4SoZ6p2+XkcHwhey2go5tXEGJ1KGcbR5NtBQpuidY5e1byccdCAdB04t89/1O/w1cDnyilFU=';
 
 $userId = 'Uffa138efe037e6e889d0b0f4a871c005';
 
